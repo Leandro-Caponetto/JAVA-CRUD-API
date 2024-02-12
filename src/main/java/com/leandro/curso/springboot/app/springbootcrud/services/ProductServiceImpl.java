9ff020a87f82006c79f924d711f92e33ca877440
Leandro-Caponetto <caponettopeppers@gmail.com>
2024-02-12 16:35:19 -0300

    curd java

---
+++ src/main/java/com/leandro/curso/springboot/app/springbootcrud/services/ProductServiceImpl.java
@@ -0,0 +1,64 @@
+package com.leandro.curso.springboot.app.springbootcrud.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.leandro.curso.springboot.app.springbootcrud.entities.Product;
+import com.leandro.curso.springboot.app.springbootcrud.repositories.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Product> findAll() {
+        return (List<Product>) repository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Product> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Product save(Product product) {
+        return repository.save(product);
+    }
+
+    @Override
+    @Transactional
+    public Optional<Product> update(Long id, Product product) {
+        Optional<Product> productOptional = repository.findById(id);
+        if (productOptional.isPresent()) {
+            Product productDb = productOptional.orElseThrow();
+
+            productDb.setName(product.getName());
+            productDb.setPrice(product.getPrice());
+            productDb.setDescription(product.getDescription());
+
+            return Optional.of(repository.save(productDb));
+        }
+        ;
+        return productOptional;
+    }
+
+    @Override
+    @Transactional
+    public Optional<Product> delete(Long id) {
+        Optional<Product> productOptional = repository.findById(id);
+        productOptional.ifPresent(productDb -> {
+            repository.delete(productDb);
+        });
+        return productOptional;
+    }
+
+}
